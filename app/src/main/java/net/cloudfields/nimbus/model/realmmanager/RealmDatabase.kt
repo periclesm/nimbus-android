d9f96b548375b399cd98bc9ec578b443a5a9c6a6
Pericles Maravelakis <periclesm@cloudfields.net>
2021-01-11 13:14:46 +0200

    Realm Instance

--- app/src/main/java/net/cloudfields/nimbus/model/realmmanager/RealmDatabase.kt
+++ app/src/main/java/net/cloudfields/nimbus/model/realmmanager/RealmDatabase.kt
@@ -1,7 +1,38 @@
 package net.cloudfields.nimbus.model.realmmanager
 
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
 class RealmDatabase {
     companion object {
         var shared = RealmDatabase()
+    }
+
+    private val schemaVer: Long = 0
+    private val dbFileName: String = "cloudDB.realm"
+    private var mainDB: Realm? = null
+
+    var db: Realm
+        set(newSchema) {
+            this.mainDB = newSchema
+        }
+        get() {
+            if (this.mainDB == null) {
+                this.mainDB = this.initDB()
+            }
+
+            return this.mainDB!!
+        }
+
+    ///Initializes the database instance to be used in the app
+    fun initDB(): Realm {
+        val config = RealmConfiguration.Builder()
+            .name(dbFileName)
+            .schemaVersion(schemaVer)
+            .deleteRealmIfMigrationNeeded()
+            .compactOnLaunch()
+            .build()
+
+        return Realm.getInstance(config)
     }
 }
