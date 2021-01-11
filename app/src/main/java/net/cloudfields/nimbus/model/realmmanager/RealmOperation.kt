de595c31225ae736fdc47fe0f1e1c57fe567465b
Pericles Maravelakis <periclesm@cloudfields.net>
2021-01-11 16:28:23 +0200

    Realm Operation and Query (CRUDs)

--- app/src/main/java/net/cloudfields/nimbus/model/realmmanager/RealmOperation.kt
+++ app/src/main/java/net/cloudfields/nimbus/model/realmmanager/RealmOperation.kt
@@ -1,7 +1,108 @@
 package net.cloudfields.nimbus.model.realmmanager
 
+import io.realm.*
+
 class RealmOperation {
     companion object {
 
+        fun get(rObject: RealmObject): RealmResults<out RealmObject>? {
+            val db = RealmDatabase.shared.db
+            return db.where(rObject::class.java).findAll()
+        }
+
+        /// Adds a single object in the database
+        fun add(rObject: RealmObject, async: Boolean = false) {
+            val db = RealmDatabase.shared.db
+
+            if (async) {
+                db.executeTransactionAsync({
+                    db.insertOrUpdate(rObject)
+                }, { error ->
+                    //do something to handle this
+                })
+            } else {
+                db.executeTransaction {
+                    db.insertOrUpdate(rObject)
+                }
+            }
+        }
+
+        /// Adds an array of objects in the database
+        fun add(objectList: List<RealmObject>, async: Boolean = false) {
+            val db = RealmDatabase.shared.db
+
+            if (async) {
+                db.executeTransactionAsync({
+                    db.insertOrUpdate(objectList)
+                }, { error ->
+                    //handle this
+                })
+            } else {
+                db.executeTransaction {
+                    db.insertOrUpdate(objectList)
+                }
+            }
+        }
+
+        /// Delete an object from the database
+        fun delete(rObject: RealmObject, async: Boolean = false) {
+            val db = RealmDatabase.shared.db
+
+            if (async) {
+                db.executeTransactionAsync({
+                    rObject.deleteFromRealm()
+                }, { error ->
+                    // handle it
+                })
+            } else {
+                db.executeTransaction {
+                    rObject.deleteFromRealm()
+                }
+            }
+        }
+
+        /// Delete an array of objects from the database
+        fun delete(objectList: RealmList<RealmObject>, async: Boolean = false) {
+            val db = RealmDatabase.shared.db
+
+            if (async) {
+                db.executeTransactionAsync({
+                    objectList.deleteAllFromRealm()
+                }, { error ->
+                    // handle it
+                })
+            } else {
+                db.executeTransaction {
+                    objectList.deleteAllFromRealm()
+                }
+            }
+        }
+
+        fun deleteAll(rObject: RealmObject, async: Boolean = false) {
+            val db = RealmDatabase.shared.db
+
+            if (async) {
+                db.executeTransactionAsync({
+                    val entries = this.get(rObject)
+                    entries?.deleteAllFromRealm()
+                }, { error ->
+                    // handle it
+                })
+            } else {
+                db.executeTransaction {
+                    val entries = this.get(rObject)
+                    entries?.deleteAllFromRealm()
+                }
+            }
+        }
+
+        /// Delete everything from the database
+        fun deleteAllData() {
+            val db = RealmDatabase.shared.db
+
+            db.executeTransactionAsync {
+                db.deleteAll()
+            }
+        }
     }
 }
--- app/src/main/java/net/cloudfields/nimbus/model/realmmanager/RealmQuery.kt
+++ app/src/main/java/net/cloudfields/nimbus/model/realmmanager/RealmQuery.kt
@@ -1,7 +1,106 @@
 package net.cloudfields.nimbus.model.realmmanager
 
+import io.realm.RealmList
+import io.realm.RealmObject
+import io.realm.RealmResults
+import io.realm.Sort
+
 class RealmQuery {
     companion object {
 
+        //MARK: - Composite Sort and Filter --
+
+        /// Under Development
+        fun filterAndSort(rObject: RealmObject, sortAttribute: String, ascending: Sort = Sort.ASCENDING): RealmResults<out RealmObject>? {
+            return null
+        }
+
+        /// Under Development
+        fun filterAndSort(dataList: RealmResults<RealmObject>, keypath: String, ascending: Sort = Sort.ASCENDING): RealmResults<out RealmObject>? {
+            return null
+        }
+
+        //MARK: - Filter
+
+        /// Under Development
+        fun filter(dataObject: RealmObject): RealmResults<RealmObject>? {
+            return null
+        }
+
+        /// Under Development
+        fun filter(dataList: RealmResults<RealmObject>): RealmResults<RealmObject>? {
+            return null
+        }
+
+        //MARK: - Sort
+
+        /**
+         * Retrieves the requested Realm objects and sorts them by the input attributes.
+         * @param rObject: The Realm object.
+         * @param sortAttribute: `String` The attribute name to be used for sorting the data.
+         * @param ascending: `Boolean` The sort type: `true` for Ascending, `false` for Descending. Default is true.
+         * @return A Realm `Results` collection with data of the requested query.
+         */
+        fun sort(rObject: RealmObject, sortAttribute: String, ascending: Sort = Sort.ASCENDING): RealmResults<out RealmObject>? {
+            return RealmOperation.get(rObject)?.sort(sortAttribute, ascending)
+        }
+
+        /**
+        Sorts the input Realm `Results` by the input attributes.
+         * @note Does not query the database. Uses `objectResults` instead.
+         * @param objectResults: The Realm `Results` (used as input)
+         * @param sortAttribute: `String` The attribute name to be used for sorting the data.
+         * @param ascending: `Boolean` The sort type: `true` for Ascending, `false` for Descending. Default is true.
+         * @return A Realm `Results` collection with data of the requested query.
+         */
+        fun sort(objectList: RealmResults<RealmObject>, sortAttribute: String, ascending: Sort = Sort.ASCENDING): RealmResults<RealmObject> {
+            return objectList.sort(sortAttribute, ascending)
+        }
+
+        /**
+        Sorts the input Realm `Results` by the input attributes.
+         * @note Does not query the database. Uses `objectResults` instead.
+         * @param objectList: The Realm `List` (used as input)
+         * @param sortAttribute: `String` The attribute name to be used for sorting the data.
+         * @param ascending: `Boolean` The sort type: `true` for Ascending, `false` for Descending. Default is true.
+         * @return A Realm `List` collection with data of the requested query.
+         */
+        fun sort(objectList: RealmList<RealmObject>, sortAttribute: String, ascending: Sort = Sort.ASCENDING): RealmList<RealmObject> {
+            val results = objectList.sort(sortAttribute, ascending)
+            return this.resultsToListConversion(results)
+        }
+
+        //MARK: - Limit
+
+        /**
+         * Limits a given Array to the (input) number of desired objects.
+         *  @param objectArray: An `Array`of objects (used as input).
+         *  @param limit: `Int` Limits the number of items returned. Only the top `limit` numbers are included.
+         *  @return A subarray with the top objects.
+         */
+        fun limit(datalist: List<Any>, limit: Int): List<Any> {
+            return if (limit < datalist.count()) {
+                datalist.subList(fromIndex = 0, toIndex = limit)
+            } else {
+                datalist
+            }
+        }
+
+        //MARK: - Utilities
+
+        /**
+         * Converts an input Realm `Results` collection to a Realm `List`.
+         */
+        fun resultsToListConversion(results: RealmResults<RealmObject>): RealmList<RealmObject> {
+            if (results.isNotEmpty()) {
+                val resultsList: RealmList<RealmObject> = RealmList()
+
+                for (rObject in results) {
+                    resultsList.add(rObject)
+                }
+            }
+
+            return RealmList()
+        }
     }
 }
